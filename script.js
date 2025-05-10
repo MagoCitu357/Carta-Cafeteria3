4e13b7e00d900790df4e506c3f94dd26ac41b37f
MagoCitu357 <137245741+MagoCitu357@users.noreply.github.com>
2025-05-10 18:28:21 -0300

    v1

--- script.js
+++ script.js
@@ -1,20 +1,29 @@
 const numText = 3
 // Entrada
-document.getElementById("precio-panAjo").textContent = `$${precios.Pan_con_ajo.toFixed(numText)}`;
-document.getElementById("precio-sopa").textContent = `$${precios.Sopa_del_día.toFixed(numText)}`;
-document.getElementById("precio-ensalada").textContent = `$${precios.Ensalada_mixta.toFixed(numText)}`;
+document.getElementById("precio-empanada").textContent = `$${precios.Empanada.toFixed(numText)}`;
+document.getElementById("precio-provoleta").textContent = `$${precios.Provoleta.toFixed(numText)}`;
+document.getElementById("precio-choripan").textContent = `$${precios.Choripan.toFixed(numText)}`;
+document.getElementById("precio-morcilla").textContent = `$${precios.Morcilla.toFixed(numText)}`;
+document.getElementById("precio-ensalada").textContent = `$${precios.Ensalada_Criolla.toFixed(numText)}`;
+document.getElementById("precio-pan").textContent = `$${precios.Pan_con_chimichurri.toFixed(numText)}`;
 // Principales:
-document.getElementById("precio-hamburguesa").textContent = `$${precios.hamburguesa.toFixed(numText)}`;
-document.getElementById("precio-pizza").textContent = `$${precios.pizza.toFixed(numText)}`;
-document.getElementById("precio-pollo").textContent = `$${precios.Pollo_plancha.toFixed(numText)}`;
-document.getElementById("precio-tacos").textContent = `$${precios.tacos.toFixed(numText)}`;
-document.getElementById("precio-pasta").textContent = `$${precios.Pasta_carbonara.toFixed(numText)}`;
+document.getElementById("precio-mNapolitana").textContent = `$${precios.Milanesa_napolitana.toFixed(numText)}`;
+document.getElementById("precio-polloPapas").textContent = `$${precios.Pollo_papas_horneado.toFixed(numText)}`;
+document.getElementById("precio-locro").textContent = `$${precios.Locro.toFixed(numText)}`;
+document.getElementById("precio-asado").textContent = `$${precios.Asado.toFixed(numText)}`;
+document.getElementById("precio-humita").textContent = `$${precios.Humita.toFixed(numText)}`;
 // Postres:
 document.getElementById("precio-helado").textContent = `$${precios.Helado.toFixed(numText)}`;
-document.getElementById("precio-tartaQueso").textContent = `$${precios.Tarta_de_queso.toFixed(numText)}`;
+document.getElementById("precio-panqueque").textContent = `$${precios.Panqueque_dulce_de_leche.toFixed(numText)}`;
 document.getElementById("precio-flan").textContent = `$${precios.Flan_casero.toFixed(numText)}`;
+document.getElementById("precio-pastelitos").textContent = `$${precios.Pastelitos.toFixed(numText)}`;
+document.getElementById("precio-chocotorta").textContent = `$${precios.Chocotorta.toFixed(numText)}`;
 // Bebidas:
+document.getElementById("precio-vino").textContent = `$${precios.Vino_tinto.toFixed(numText)}`;
+document.getElementById("precio-cerveza").textContent = `$${precios.Cerveza.toFixed(numText)}`;
+document.getElementById("precio-fernet").textContent = `$${precios.Fernet_con_coca.toFixed(numText)}`;
+document.getElementById("precio-refresco_CocaCola").textContent = `$${precios.Refresco_CocaCola.toFixed(numText)}`;
+document.getElementById("precio-refresco_sprite").textContent = `$${precios.Refresco_Sprite.toFixed(numText)}`;
 document.getElementById("precio-agua").textContent = `$${precios.Agua.toFixed(numText)}`;
-document.getElementById("precio-zumo").textContent = `$${precios.Zumo_natural.toFixed(numText)}`;
-document.getElementById("precio-refresco").textContent = `$${precios.Refresco.toFixed(numText)}`;
-document.getElementById("precio-cafe").textContent = `$${precios.Café.toFixed(numText)}`;
+document.getElementById("precio-zumo").textContent = `$${precios.Jugo_natural.toFixed(numText)}`;
+document.getElementById("precio-cafe").textContent = `$${precios.Cafe.toFixed(numText)}`;
